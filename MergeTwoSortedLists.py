3f4774bf83c3c8c9062017654f5d249a80e779db
v-akkala <akkala@umich.edu>
2026-07-01 14:36:57 -0400

    Merge Two Sorted Lists

--- MergeTwoSortedLists.py
+++ MergeTwoSortedLists.py
@@ -11,35 +11,28 @@ class Solution:
             return list2
         if not list2:
             return list1
-        if(list1.val < list2.val):
-            start = list1
-        else:
-            start = list2
-        l1prev = list1
-        l2prev = list2
-        while list1 and list2:
-            if list1.next:
-                if list1.next.val <= list2.val:
-                    list1 = list1.next
+        if list1.val > list2.val:
+            temp = list1
+            list1 = list2
+            list2 = temp
+        l1prev = None
+        cur = list1
+        l1next = list1.next
+        start = list1
+        while list2 and l1next:
+            if l1next.val < list2.val:
+                    l1prev = cur
+                    cur = l1next
+                    l1next = l1next.next
                     continue
-            elif list2.next:
-                if list2.next.val <= list1.val:
-                    list2 = list2.next
-                    continue
-            if list1.val <= list2.val:
-                l1next = list1.next
-                list1.next = list2
-                list1 = l1next
-                continue
-            if list1.val > list2.val:
+            if cur.val <= list2.val:
+                l1prev = cur
                 l2next = list2.next
-                list2.next = list1
+                cur.next = list2
+                list2.next = l1next
+                cur = list2
                 list2 = l2next
                 continue
-        if not (list1 or list2):
-            return start
-        if not list1:
-            list1.next = list2
-        if not list2:
-            list2.next = list1
+        if not l1next:
+            cur.next = list2
         return start
