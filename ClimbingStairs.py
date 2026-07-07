93d4406bae15653c8df1121c01cb26c881b3a5aa
v-akkala <akkala@umich.edu>
2026-07-06 22:37:04 -0400

    Climbing Stairs TLE

--- ClimbingStairs.py
+++ ClimbingStairs.py
@@ -2,6 +2,6 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         if(n == 1):
             return 1
-        elif(n = 2):
+        elif(n == 2):
             return 2
         return self.climbStairs(n - 1) + self.climbStairs(n - 2)
