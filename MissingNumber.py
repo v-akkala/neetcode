8bee59629ec328274f353b3b54a18c5b7ce1b997
v-akkala <akkala@umich.edu>
2026-07-07 19:40:09 -0400

    Missing Number

--- MissingNumber.py
+++ MissingNumber.py
@@ -1,3 +1,7 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        
+        nums.sort()
+        for x in range(0, len(nums)):
+            if nums[x] ^ x != 0:
+                return x
+        return len(nums)
