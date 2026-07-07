22c315f01f5d26307ebe69e5b33f9e5d9fb7e8d3
v-akkala <akkala@umich.edu>
2026-07-06 22:10:29 -0400

    Subtree of Another Tree

--- SubtreeOfAnotherTree.py
+++ SubtreeOfAnotherTree.py
@@ -6,10 +6,42 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        if not root or not subRoot:
-            return
-        if cur.val == subRoot.val:
-            return True and isSubtree(cur.left, subRoot.left) and isSubtree(cur.right,subRoot.right)
-        if cur.val != subRoot.val:
-            return False or isSubtree(cur.left, subRoot.left) or isSubtree(cur.right, subRoot.right)
-        
+        if root and (subRoot == None):
+            return False
+        elif subRoot and (root == None):
+            return False
+        if not (root or subRoot):
+            return True
+        if root.val == subRoot.val:                    
+            if self.isSameTree(root, subRoot):
+                return True
+            else:
+                return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+        if root.val != subRoot.val:
+            return False or self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+
+
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        pnodes = deque([p])
+        porder = []
+        qnodes = deque([q])
+        qorder = []
+        while pnodes:
+            cur = pnodes.popleft()
+            if cur == None:
+                porder.append(None)
+            else:
+                porder.append(cur.val)
+            if cur:
+                pnodes.append(cur.left)
+                pnodes.append(cur.right)
+        while qnodes:
+            cur = qnodes.popleft()
+            if cur == None:
+                qorder.append(None)
+            else:
+                qorder.append(cur.val)
+            if cur:
+                qnodes.append(cur.left)
+                qnodes.append(cur.right)
+        return qorder == porder
