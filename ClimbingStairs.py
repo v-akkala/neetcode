affcd48e089bbbed37fe526596de19fda4298518
v-akkala <akkala@umich.edu>
2026-07-06 22:57:58 -0400

    Climbing Stairs

--- ClimbingStairs.py
+++ ClimbingStairs.py
@@ -1,7 +1,15 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if(n == 1):
+        self.memo = [0] * n
+        return self.Climber(n)
+        
+    def Climber(self, n: int) -> int:
+        if n == 1:
             return 1
-        elif(n == 2):
-            return 2
-        return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        self.memo[0], self.memo[1] = 1, 2
+        if n == 0:
+            return 0
+        if self.memo[n-1] > 0:
+            return self.memo[n-1]
+        self.memo[n-1] = self.Climber(n - 1) + self.Climber(n - 2)
+        return self.memo[n-1]
