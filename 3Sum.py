bff945a6960cff0a6109ad760a6bc0fe020f0f1c
v-akkala <akkala@umich.edu>
2026-07-08 19:55:27 -0400

    3Sum

--- 3Sum.py
+++ 3Sum.py
@@ -1,20 +1,27 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
         nums.sort()
-        ans = []
+        ans = set()
         for x in range(0, len(nums) - 1):
             lidx = x + 1
             low = nums[lidx]
             hidx = len(nums) - 1
             high = nums[hidx]
             while hidx != lidx:
-                if hidx != lidx:
-                    if nums[x] + low + high == 0:
-                        ans.append([nums[x], low, high])
-                if abs(low) > abs(high):
-                    lidx += 1
-                    low = nums[lidx]
+                if nums[x] + low + high == 0:
+                    ans.add((nums[x], low, high))
+                if nums[x] <= 0:
+                    if low + high < -1 * nums[x]:
+                        lidx += 1
+                        low = nums[lidx]
+                    else:
+                        hidx -= 1
+                        high = nums[hidx]
                 else:
-                    hidx -= 1
-                    high = nums[hidx]
-        return ans
+                    if low + high > -1 * nums[x]:
+                        lidx += 1
+                        low = nums[lidx]
+                    else:
+                        hidx -= 1
+                        high = nums[hidx]
+        return list(ans)
