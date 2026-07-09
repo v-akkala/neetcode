315aaae5f6f38dcb73541fd9800edc2bb02bea08
v-akkala <akkala@umich.edu>
2026-07-09 14:28:55 -0400

    Longest Repeating Character Replacement w/ O(26n)

--- LongestRepeatingCharacterReplacement.py
+++ LongestRepeatingCharacterReplacement.py
@@ -7,7 +7,7 @@ class Solution:
         while right < len(s):
             chardict[s[right]] = chardict.get(s[right], 0) + 1
             while (sum(1 for val in chardict.values() if val > k) <= 1 
-            and sum(val for key, val in chardict.items() if key != s[left])) > k:
+            and sum(val for key, val in chardict.items() if key != max(chardict, key=chardict.get))) > k:
                 ans = max(ans, right - left)
                 chardict[s[left]] = chardict[s[left]] - 1
                 left += 1
