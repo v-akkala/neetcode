7c30f71e892e6937be645f5a06b924f13b96edd1
v-akkala <akkala@umich.edu>
2026-07-09 17:57:54 -0400

    Remove Nth Node From End of List (Two Pass)

--- RemoveNthNodeFromEndOfList.py
+++ RemoveNthNodeFromEndOfList.py
@@ -5,4 +5,22 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        
+        cur = head
+        end = head
+        prev = head
+        next = head.next
+        counter = 0
+        while end:
+            counter += 1
+            end = end.next
+        stravel = counter - n
+        for x in range(0, stravel):
+            prev = cur
+            cur = cur.next
+            next = cur.next
+        if stravel == 0:
+            return head.next
+        prev.next = next
+        return head
+
+
