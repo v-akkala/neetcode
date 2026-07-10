30086f682add851dae6b22f9206f69a6c980e423
v-akkala <akkala@umich.edu>
2026-07-10 13:27:02 -0400

    LCA of BST

--- LowestCommonAncestorInBST.py
+++ LowestCommonAncestorInBST.py
@@ -7,4 +7,16 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        
+        if p.val > q.val:
+            higher = p
+            lower = q
+        else: 
+            higher = q
+            lower = p
+        while not (root.val <= higher.val and root.val >= lower.val):
+            if root.val > higher.val and root.val > lower.val:
+                root = root.left
+            elif root.val < higher.val and root.val < lower.val:
+                root = root.right
+        return root
+
