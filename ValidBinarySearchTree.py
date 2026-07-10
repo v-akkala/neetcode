07f1c80345742cb4f809c955109b28a515fbf24a
v-akkala <akkala@umich.edu>
2026-07-10 14:38:56 -0400

    Valid BST

--- ValidBinarySearchTree.py
+++ ValidBinarySearchTree.py
@@ -1,4 +1,3 @@
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -10,18 +9,32 @@ class Solution:
         if not (root.left or root.right):
             return True
         if not root.left:
-            if root.right.val > root.val:
+            if self.higherValidator(root.right, root.val):
                 return True and self.isValidBST(root.right)
             else:
                 return False
         elif not root.right:
-            if root.left.val < root.val:
+            if self.lowerValidator(root.left, root.val):
                 return True and self.isValidBST(root.left)
             else:
                 return False
-        if root.left.val < root.val and root.right.val > root.val:
+        if self.lowerValidator(root.left, root.val) and self.higherValidator(root.right, root.val):
             return True and self.isValidBST(root.left) and self.isValidBST(root.right)
         else:
             return False
 
-    
+    def lowerValidator(self, root: Optional[TreeNode], val: int) -> bool:
+        if not root:
+            return True
+        if root.val < val:
+            return True and self.lowerValidator(root.left, val) and self.lowerValidator(root.right, val)
+        else:
+            return False
+    def higherValidator(self, root: Optional[TreeNode], val: int) -> bool:
+        if not root:
+            return True
+        if root.val > val:
+            return True and self.higherValidator(root.left, val) and self.higherValidator(root.right, val)
+        else:
+            return False
+
