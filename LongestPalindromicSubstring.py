be5ea5275fb3907015ae6ec1c6d7d7c4562d4748
v-akkala <akkala@umich.edu>
2026-07-15 19:38:51 -0400

    Longest Palindromic Substring Two Pointers

--- LongestPalindromicSubstring.py
+++ LongestPalindromicSubstring.py
@@ -1,3 +1,30 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        
+        ans = s[0]
+        for i in range(0, len(s) - 1):
+            exp = 1
+            cur = 1
+            while exp <= i and exp + i <= len(s) - 1:
+                if s[i + exp] == s[i - exp]:
+                    cur += 2
+                else:
+                    break
+                exp += 1
+            exp -= 1
+            if len(s[i - exp: i + exp + 1]) > len(ans):
+                ans = s[i - exp: i + exp + 1]
+            cur = 0
+            left = i
+            right = i + 1
+            while left >= 0 and right <= len(s) - 1:
+                if s[left] == s[right]:
+                    cur += 2
+                else:
+                    break
+                left -= 1
+                right += 1
+            left += 1
+            right -= 1
+            if len(s[left:right+1]) > len(ans):
+                ans = s[left:right+1]
+        return ans
