4519b6c9da570b26d90f2ebad7b15aa24c961fe5
v-akkala <akkala@umich.edu>
2026-07-15 20:01:13 -0400

    Longest Palindromic Substring

--- LongestPalindromicSubstring.py
+++ LongestPalindromicSubstring.py
@@ -3,23 +3,17 @@ class Solution:
         ans = s[0]
         for i in range(0, len(s) - 1):
             exp = 1
-            cur = 1
             while exp <= i and exp + i <= len(s) - 1:
-                if s[i + exp] == s[i - exp]:
-                    cur += 2
-                else:
+                if s[i + exp] != s[i - exp]:
                     break
                 exp += 1
             exp -= 1
             if len(s[i - exp: i + exp + 1]) > len(ans):
                 ans = s[i - exp: i + exp + 1]
-            cur = 0
             left = i
             right = i + 1
             while left >= 0 and right <= len(s) - 1:
-                if s[left] == s[right]:
-                    cur += 2
-                else:
+                if s[left] != s[right]:
                     break
                 left -= 1
                 right += 1
