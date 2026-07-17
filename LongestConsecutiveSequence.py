79771ee34a943887021ced98f7fc7cc4d21eb650
v-akkala <akkala@umich.edu>
2026-07-17 14:49:03 -0400

    Longest Consecutive Sequence

--- LongestConsecutiveSequence.py
+++ LongestConsecutiveSequence.py
@@ -2,13 +2,16 @@ class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         consec = {}
         longest = 0
+        numset = set(nums)
+        nums = list(numset)
         for i in range(len(nums)):
             consec[nums[i] + 1] = i
         for j in range(len(nums)):
             curlong = 1
             curidx = j
-            while nums[curidx] in consec:
-                curlong += 1
-                curidx = consec[nums[curidx]]
+            if nums[curidx] in consec and nums[curidx] + 1 not in numset:
+                while nums[curidx] in consec:
+                    curlong += 1
+                    curidx = consec[nums[curidx]]
             longest = max(curlong, longest)
         return longest
