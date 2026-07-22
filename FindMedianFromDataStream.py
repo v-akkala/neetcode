73b2281736bf086d2f1700223cdf249feeb4c706
v-akkala <akkala@umich.edu>
2026-07-21 21:31:20 -0400

    Find Med Temp

---
+++ FindMedianFromDataStream.py
@@ -0,0 +1,18 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.medheap = []
+        self.nums = []
+
+    def addNum(self, num: int) -> None:
+        nums.append(num)
+        if len()
+
+    def findMedian(self) -> float:
+        
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
