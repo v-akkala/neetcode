d91fc2ce815f0c60e6741644873784759b844a82
v-akkala <akkala@umich.edu>
2026-07-22 20:12:27 -0400

    Design Add and Search Words DS

--- DesignAdd&SearchWordsDS.py
+++ DesignAdd&SearchWordsDS.py
@@ -19,37 +19,19 @@ class WordDictionary:
     def search(self, word: str) -> bool:
         cur = self.root
         i = 0
-        while i < len(word):
-            if word[i] != ".":
-                if word[i] in cur.children:
-                    cur = cur.children[word[i]]
-                else:
-                    return False
-            else:
-                c = 0
-                while word[i + c] == ".":
-                    c += 1
-                    if i + c >= len(word) - 1: 
-                        return True
-                recursedots(word, i + 1)
-                if self.ans:
+        return self.recursedots(word, i, cur)
+
+    def recursedots(self, word, index, node):
+        if index == len(word):
+            return node.endofword
+        if word[index] == ".":
+            for children in node.children.values():
+                if self.recursedots(word, index + 1, children):
                     return True
-                i += c
-            i += 1
-        return cur.endofword
-    
-    def recursedots(self, word, index):
-        self.ans = False
-        if index >= len(word) - 1:
-            self.ans = True
-            break
-        for node in cur.children.values():
-            if word[index] == ".":
-                backtrack(word, index + 1)
-            elif word[index] in node.children:
-                cur = node.children[word[index]]
-                i += 1
-                break
+            return False
+        else:
+            if word[index] in node.children:
+                return self.recursedots(word, index + 1, node.children[word[index]])
             else:
                 return False
 
