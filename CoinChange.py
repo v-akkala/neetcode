6d8ce08b153a159ceca68a828577925447668daa
v-akkala <akkala@umich.edu>
2026-07-24 17:00:35 -0400

    Coin Change

---
+++ CoinChange.py
@@ -0,0 +1,23 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        change = [-1] * (amount + 1)
+        coins = sorted(coins)
+        change[0] = 0
+        for coin in coins:
+            if coin <= amount:
+                change[coin] = 1
+
+        for x in range(1, amount + 1):
+            temp = []
+            if change[x] == 1:
+                continue
+            for coin in coins:
+                if x - coin < 1:
+                    continue
+                if change[x - coin] != -1:
+                    temp.append(change[x - coin] + 1)
+            if temp:
+                change[x] = min(temp)
+                
+        return change[amount]
+
