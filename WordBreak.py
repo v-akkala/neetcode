616c82d3d17e9291ce4982be49638b5a911e74f9
v-akkala <akkala@umich.edu>
2026-07-24 20:47:23 -0400

    Word Break

--- WordBreak.py
+++ WordBreak.py
@@ -1,15 +1,16 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        wordbreaks = [False] * (len(s) + 1)
+        wordbreaks = [False] * len(s) + 1
         wordbreaks[0] = True
         maxlen = 0
-        for word in wordDict:
+        for word in wordbreaks:
             maxlen = max(maxlen, len(word))
-        for i in range(1, len(s) + 1):
-            for j in range(max(0, i-maxlen), i):
-                if wordbreaks[j] == True:
-                    curword = s[j:i]
+        for i in range(1, len(s)):
+            for j in range(i, max(-1, i - maxlen), -1)
+                if wordbreaks[j] = True:
+                    curword = s[j + 1:i + 1]
                     if curword in wordDict:
                         wordbreaks[i] = True
+                        break
         return wordbreaks[len(s)]
 
