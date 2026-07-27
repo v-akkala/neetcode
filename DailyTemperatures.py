bd17cd3801057b72ef13d8179a42802ebb43c94c
v-akkala <akkala@umich.edu>
2026-07-26 20:31:29 -0400

    Daily Temperature

---
+++ DailyTemperatures.py
@@ -0,0 +1,11 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = [0]
+        ans = [0] * len(temperatures)
+        for i in range(1, len(temperatures)):
+            while stack and temperatures[stack[-1]] < temperatures[i]:
+                idx = stack.pop()
+                ans[idx] = i - idx
+            stack.append(i)
+        return ans
+
