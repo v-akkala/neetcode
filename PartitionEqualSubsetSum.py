321d24207d95d26ba10f97d5e8577178f555fb68
v-akkala <akkala@umich.edu>
2026-07-29 18:15:31 -0400

    Partition Equal Subset Sum

--- PartitionEqualSubsetSum.py
+++ PartitionEqualSubsetSum.py
@@ -3,15 +3,27 @@ class Solution:
         numsum = sum(nums)
         if numsum % 2 != 0:
             return False
-        target = numsum / 2
-        self.ans = False
-        self.backtrack(nums, 0, 0, target)
-        return self.ans
 
-    def backtrack(self, nums, cursum, index, target):
-        if cursum == target:
-            self.ans = True
-        elif cursum > target:
-            return
-        for i in range(index, len(nums)):
-            self.backtrack(nums, cursum + nums[i], i + 1, target)
+        target = numsum // 2
+        dp = [([None] * (target + 1)) for _ in range(len(nums) + 1)]
+        dp[0][0] = True
+        for i in range(1, len(dp[0])):
+            dp[0][i] = False
+
+        def dyp(i, j):
+            if i == 0:
+                return dp[0][j]
+            if dp[i][j]:
+                return True
+            elif dp[i][j] == False:
+                return False
+            else:
+                if j - nums[i - 1] < 0:
+                    dp[i][j] = dyp(i - 1,j)
+                else:
+                    dp[i][j] = dyp(i - 1,j) or dyp(i - 1,j - nums[i - 1])
+            return dp[i][j]
+
+        dyp(len(nums), target)
+
+        return dp[len(nums)][target]
