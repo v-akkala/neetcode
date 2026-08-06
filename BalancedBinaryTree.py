c3e94442813168637970fd74c56d5de3302569e3
v-akkala <akkala@umich.edu>
2026-08-06 19:52:27 -0400

    Balanced Binary Tree

--- BalancedBinaryTree.py
+++ BalancedBinaryTree.py
@@ -6,4 +6,21 @@
 #         self.right = right
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        
+        if not root:
+            return True
+        if abs(self.getHeight(root.left) - self.getHeight(root.right)) > 1:
+            return False
+        else:
+            return True and self.isBalanced(root.left) and self.isBalanced(root.right)
+
+    def getHeight(self, root):
+                if not root:
+                    return 0
+                elif not (root.left or root.right):
+                    return 1
+                elif not root.left:
+                    return 1 + self.getHeight(root.right)
+                elif not root.right:
+                    return 1 + self.getHeight(root.left)
+                return 1 + max(self.getHeight(root.left), self.getHeight(root.right))
+
