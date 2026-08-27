beee3fb341d73e13321dcc9f61b4f3573184f269
v-akkala <akkala@umich.edu>
2026-08-27 14:30:21 -0400

    Min Stack

---
+++ MinStack.py
@@ -0,0 +1,24 @@
+class MinStack:
+
+    def __init__(self):
+        
+
+    def push(self, value: int) -> None:
+        
+
+    def pop(self) -> None:
+        
+
+    def top(self) -> int:
+        
+
+    def getMin(self) -> int:
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(value)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
